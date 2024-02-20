73e25441657485b33c98f715aa8ceb1f326f2f8b
Rafael Cruz <rcas2009@outlook.com>
2024-02-19 22:02:24 -0300

    Create Find the Difference.py

---
+++ Programming Skills/Find the Difference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for i in t:
+            if i in s:
+                t = t.replace(i, "", 1)
+                s = s.replace(i, "", 1)
+        return t
