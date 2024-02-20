aab1fdd0241758cc3231f805cb8c7fabb0251333
Rafael Cruz <rcas2009@outlook.com>
2024-02-19 22:36:30 -0300

    Pandas: Find Customer Referee.py

---
+++ Pandas/Find Customer Referee.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    output = pd.DataFrame({"name": customer[(customer["referee_id"].isnull()) | (customer["referee_id"]!= 2)]["name"]})
+    return output
