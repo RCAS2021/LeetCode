278ac5835851da8bc26217aba991aea2ae90ddb5
Rafael Cruz <rcas2009@outlook.com>
2024-02-19 22:45:01 -0300

    Pandas: Create: Product Sales Analysis I.py, Modify: Find Customer Referee.py

--- Pandas/Find Customer Referee.py
+++ Pandas/Find Customer Referee.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    output = pd.DataFrame({"name": customer[(customer["referee_id"].isnull()) | (customer["referee_id"]!= 2)]["name"]})
+    output = pd.DataFrame({
+            "name": customer[(customer["referee_id"].isnull()) | (customer["referee_id"]!= 2)]["name"]
+        })
     return output
