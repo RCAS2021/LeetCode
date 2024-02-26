498eb9f58dba54303a567ac94a2b8172d7a319b9
Rafael Cruz <rcas2009@outlook.com>
2024-02-26 11:56:28 -0300

    Programming Skills: Modify: Comment Robot Bounded in Circle.py

--- Programming Skills/Robot Bounded in Circle.py
+++ Programming Skills/Robot Bounded in Circle.py
@@ -1,17 +1,24 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
+        # Initializes direction facing north
         direction = (0, 1)
-        initial_pos = [0, 0]
+        # Initializes position
+        pos = [0, 0]
 
+        # For each insctruction
         for i in instructions:
+            # If instruction = G, pos.x and pos.y += dir.x and dir.y
             if i == "G":
-                initial_pos[0] += direction[0]
-                initial_pos[1] += direction[1]
+                pos[0] += direction[0]
+                pos[1] += direction[1]
+            # If instruction = L, dir.x = -dir.y and dir.y = dir.x
             elif i == "L":
                 direction = (-direction[1], direction[0])
+            # If instruction = R, dir.x = dir.y and dir.y = -dir.x
             elif i == "R":
                 direction = (direction[1], -direction[0])
 
-        print(direction)
-        print(initial_pos)
-        return initial_pos == [0, 0] or direction != (0, 1)
+            print(f"after {i}, new pos ={pos}")
+            print(f"after {i}, new dir ={direction}")
+        # If pos == [0, 0] (initial position) or direction != (0, 1) (north)
+        return pos == [0, 0] or direction != (0, 1)
